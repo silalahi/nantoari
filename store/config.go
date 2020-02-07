13c03bcee87fac53d01bd6e7399939f2093c15d8
Jogi Silalahi <jsilalahi@dyned.com>
2020-02-07 13:39:18 +0700

    wip

--- store/config.go
+++ store/config.go
@@ -4,22 +4,29 @@ import (
 	"fmt"
 )
 
-// Config is a struct to represent a key/value store (Redis)
+// Config is a struct to represent a key/value store
 type Config struct {
-	Prefix string      `yaml:"prefix"`
-	Driver string      `yaml:"driver"`
-	Redis  RedisConfig `yaml:"redis"`
+	Prefix   string         `yaml:"prefix"`
+	Driver   string         `yaml:"driver"`
+	Redis    RedisConfig    `yaml:"redis"`
+	InMemory InMemoryConfig `yaml:"inmemory"`
 }
 
-// RedisConfig is a struct contain Redis configuration
+// RedisConfig is a struct contain store configuration for Redis implementation
 type RedisConfig struct {
-	Host     string `yaml:"host"`
-	Port     int    `yaml:"port"`
-	Password string `yaml:"password"`
-	DB       int    `yaml:"db"`
+	Host       string `yaml:"host"`
+	Port       int    `yaml:"port"`
+	Password   string `yaml:"password"`
+	DB         int    `yaml:"db"`
+	Expiration int    `yaml:"expiration"`
+}
+
+// InMemoryConfig is a struct contain store configuration for In Memory implementation
+// The InMemory driver should be used for development only since the data not persist
+type InMemoryConfig struct {
 }
 
 // Addr returns Redis address in string format
-func (rc RedisConfig) Addr() string {
-	return fmt.Sprint(rc.Host, ":", rc.Port)
+func (r RedisConfig) Addr() string {
+	return fmt.Sprint(r.Host, ":", r.Port)
 }
