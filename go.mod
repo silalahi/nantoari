a45f6b8ace5090c05ab2943466a1ee8f406def59
Jogi Silalahi <jsilalahi@dyned.com>
2020-02-07 14:18:40 +0700

    wip

--- go.mod
+++ go.mod
@@ -5,7 +5,7 @@ require (
 	github.com/go-playground/universal-translator v0.16.0 // indirect
 	github.com/go-redis/redis v6.15.1+incompatible
 	github.com/google/uuid v1.1.0
-	github.com/labstack/echo/v4 v4.0.0
+	github.com/labstack/echo/v4 v4.1.14
 	github.com/leodido/go-urn v1.1.0 // indirect
 	github.com/onsi/ginkgo v1.11.0 // indirect
 	github.com/onsi/gomega v1.8.1 // indirect
--- server/config.go
+++ server/config.go
@@ -3,9 +3,20 @@ package server
 const (
 	// DefaultServerPort is the default port of the application server
 	DefaultServerPort = 8080
+	// DefaultServerHost is the default host of the application server
+	DefaultServerHost = "localhost"
 )
 
 // Config is a struct to configure server
 type Config struct {
-	Port int `yaml:"port"`
+	Host string `yaml:"host"`
+	Port int    `yaml:"port"`
+}
+
+// DefaultConfig returns a default config instance
+func DefaultConfig() *Config {
+	return &Config{
+		Host: DefaultServerHost,
+		Port: DefaultServerPort,
+	}
 }
